8f674ef03145255bae545405ca9a0c106a7cf6fb
2EZHenry <lchoo9294@gmail.com>
2023-07-16 23:09:57 +0800

    fix: added check for pathname null

--- app/[lang]/components/locale-switcher.tsx
+++ app/[lang]/components/locale-switcher.tsx
@@ -18,7 +18,7 @@ export default function LocaleSwitcher() {
       {i18n.locales.map(locale => {
         return (
           <div
-            className={`${pathName.includes(locale) ? "font-bold" : ""} uppercase mr-2 pr-2 border-r-2 border-solid border-gray-400 last:border-r-0`}
+            className={`${pathName?.includes(locale) ? "font-bold" : ""} uppercase mr-2 pr-2 border-r-2 border-solid border-gray-400 last:border-r-0`}
             key={locale}
           >
             <Link href={redirectedPathName(locale)}>{locale}</Link>
